4fbbdb189aae8a78f2fe2599595b89684b5b4940
prithwiraj-msspl <prithwiraj.bhadra@massoftind.com>
2026-02-18 07:34:49 +0530

    database log 3

--- app/tests/integration/worker.integration.test.ts
+++ app/tests/integration/worker.integration.test.ts
@@ -89,7 +89,7 @@ describe("Test 1: End-to-End Image Processing", () => {
                 }
 
 
-                const file_status = await waitForJobStatus(insert_to_test_file_obj.image_id, "resized", 30000);
+                const file_status = await waitForJobStatus(insert_to_test_file_obj.image_id, "resized", 10000);
                 // const file_status = await filesModel.findByAny({image_id: insert_to_test_file_obj.image_id});
                 console.log("file_status", file_status)
                 expect(file_status).toBe(true);
